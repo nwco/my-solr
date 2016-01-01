cefc8b79e708a809b22e5bd2465bc42eeec706df
Nick White <nwhite@cloudera.com>
2016-01-01 14:41:03 -0700

    Updated for the Berkshelf Way

--- metadata.rb
+++ metadata.rb
@@ -1,2 +1,11 @@
 name "solr"
+maintainer "Nick White"
+maintainer_email "nwhite@cloudera.com"
+description "Installs and configures a single Solr node for testing"
 version "0.1.0"
+
+depends "yum", ">= 3.8.2"
+
+%w{ centos redhat }.each do |os|
+  supports os
+end
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: solr
+#
+# Copyright (c) 2015 Nick White, All Rights Reserved.
+include_recipe "solr::solr_server"
